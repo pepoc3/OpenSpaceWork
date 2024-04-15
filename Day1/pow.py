691e26843604ea7d013662928faa76a8e7323b30
asds <996473441@qq.com>
2024-04-15 19:15:04 +0800

    Update pow.py

--- Day1/pow.py
+++ Day1/pow.py
@@ -1,24 +1,25 @@
 import hashlib
 import time
 
-def calculate_hash(nonce):
-    return hashlib.sha256((nickname + str(nonce)).encode()).hexdigest()
-nonce1=int(input("请输入nonce:"))
-def find_hash(prefix):
-    nonce = nonce1
+def calculate_hash(nonce, nickname):
+    data = nickname + str(nonce)
+    return hashlib.sha256(data.encode()).hexdigest()
+nickname1=input("请输入昵称:")
+def find_nonce(prefix_zeros):
+    nickname = nickname1  # 请将YourNickname替换为你的昵称
+    nonce = 0
     start_time = time.time()
     while True:
-        hash_result = calculate_hash(nonce)
-        if hash_result.startswith(prefix):
+        hash_result = calculate_hash(nonce, nickname)
+        if hash_result.startswith('0' * prefix_zeros):
             end_time = time.time()
-            print(f"找到哈希值 '{hash_result}'，用时: {end_time - start_time} 秒")
+            print(f"Found hash with {prefix_zeros} leading zeros: {hash_result}")
+            print(f"Found nonce {nonce}")
+            print(f"Time taken: {end_time - start_time:.6f} seconds")
             break
         nonce += 1
 
-nickname = input("请输入昵称：")
-
-# 找到4个0开头的哈希值
-find_hash("0000")
-
-# 找到5个0开头的哈希值
-find_hash("00000")
+# 寻找4个0开头的哈希值
+find_nonce(4)
+# 寻找5个0开头的哈希值
+find_nonce(5)
