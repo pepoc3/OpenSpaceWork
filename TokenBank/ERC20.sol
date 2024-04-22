b65000d7bb51828ca1bedb35dbf000cc9a68cff3
asds <996473441@qq.com>
2024-04-22 17:47:56 +0800

    Update ERC20.sol

--- TokenBank/ERC20.sol
+++ TokenBank/ERC20.sol
@@ -1,1 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        // set name,symbol,decimals,totalSupply
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 100000000*(10**decimals);
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balances[msg.sender], "ERC20: transfer amount exceeds balance");
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowances[_from][msg.sender], "ERC20: transfer amount exceeds allowance");
+        require(_value <= balances[_from], "ERC20: transfer amount exceeds balance");
+        allowances[_from][msg.sender] -= _value;
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {    
+        return allowances[_owner][_spender];
+    }
+}
