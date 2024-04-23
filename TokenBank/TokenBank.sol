6bdbefeff6b8d3839568a4bbf8c8b71bb1652fa1
asds <996473441@qq.com>
2024-04-23 11:24:39 +0800

    Update TokenBank.sol

--- TokenBank/TokenBank.sol
+++ TokenBank/TokenBank.sol
@@ -5,14 +5,15 @@ import "./ERC20.sol";
 contract TokenBank {
 
     mapping(address => uint256) public userBalances;
-    
-    function deposit(address _erc20addr, uint256 _value) public {
+    address _erc20addr = 0x9396B453Fad71816cA9f152Ae785276a1D578492; //erc20合约地址
+
+    function deposit(uint256 _value) public {
 
         ERC20(_erc20addr).transferFrom(msg.sender, address(this), _value);
         userBalances[msg.sender] += _value;
     }
 
-    function withdraw(address _erc20addr, uint256 _value) public {
+    function withdraw(uint256 _value) public {
         require(_value <= userBalances[msg.sender], "transfer amount exceeds balance");
         ERC20(_erc20addr).transfer(msg.sender, _value);
         userBalances[msg.sender] -= _value;
