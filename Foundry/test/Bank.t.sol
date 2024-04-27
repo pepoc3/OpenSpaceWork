ab66d1816501c1edc362c3873c0282444f54cb5f
pepoc3 <email@example.com>
2024-04-27 17:25:52 +0800

    Modify Bank.t.sol

--- Foundry/test/Bank.t.sol
+++ Foundry/test/Bank.t.sol
@@ -11,15 +11,22 @@ contract BankTest is Test {
     function setUp() public {
         bk = new Bank();
     }
+    event Deposit(address indexed user, uint amount);
     function testDepositETH() public {
-        
-        uint256 amount = 100;
+        deal(alice, 1 ether);
+        // 检查事件输出是否符合预期
         vm.prank(alice);
-        bk.depositETH(alice, amount);
-          // 检查事件输出是否符合预期
-        
+        vm.expectEmit();
+        emit Deposit(alice, 1 ether);
+        bk.depositETH{value: 1 ether}();
         // 检查 balanceOf 余额更新是否符合预期
-        assertEq(bk.balanceOf(alice), amount, "Balance should be updated correctly");
+        assertEq(bk.balanceOf(alice), 1 ether, "Balance should be updated correctly");
+    }
+    //测试require(amount > 0, "Deposit amount must be greater than 0");
+    function testDepositETH_Require() public {
+        vm.prank(alice);
+        vm.expectRevert("Deposit amount must be greater than 0");
+        bk.depositETH{value: 0}();
     }
     
     
