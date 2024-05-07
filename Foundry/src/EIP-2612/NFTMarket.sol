311ad790b52d41ec0292ec486799a7f2d364ef91
pepoc3 <996473441@qq.com>
2024-05-07 11:30:09 +0800

    Modify NFTMarket.sol

--- Foundry/src/EIP-2612/NFTMarket.sol
+++ Foundry/src/EIP-2612/NFTMarket.sol
@@ -5,21 +5,26 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ERC2612-NFT.sol";
 import "./ERC2612.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 
 
 contract NFTMarket {
-    address tokenAddress;
-    address nftAddress;
-
+    IERC721 public nftContract;
+    IERC20 public tokenContract;
     struct  Listing {
         address seller;
         uint256 price;
     }
+    //白名单
+    address public alice;
+    
     mapping(uint256 => uint256) prices;
     mapping(uint256 => address) sellers;
     mapping(uint256 => Listing) public listings;
     mapping(address => uint256) public userBalances;
-    // address _erc20addr=0xb7bb1792BBfabbA361c46DC5860940e0E1bFb4b9;  
     event NFTListed(uint256 indexed tokenId, address indexed seller, uint256 price);
     event NFTSold(uint256 indexed tokenId, address indexed buyer, uint256 price);
     event Sold(address seller, address buyer, uint256 price);
@@ -28,6 +33,7 @@ contract NFTMarket {
     constructor(address _nftContract, address _tokenContract) {
         nftContract = IERC721(_nftContract);
         tokenContract = IERC20(_tokenContract);
+        alice = msg.sender;
     }
 
     function getTokenPrice(uint256 _tokenId)  public view returns (uint256) {
@@ -43,7 +49,7 @@ contract NFTMarket {
         emit NFTListed(_tokenId, msg.sender, _price);
     }
 
-    function buyNFT(uint256 _tokenId) external {
+    function buyNFT(uint256 _tokenId) internal {
         Listing memory listing = listings[_tokenId];
         require(listing.seller != address(0), "NFT is not listed for sale");
 
@@ -57,49 +63,19 @@ contract NFTMarket {
         emit NFTSold(_tokenId, msg.sender, price);
     }
 
-     function permitBuy(
-        address from,
-        address to,
-        uint price,
-        uint nftId,
-        uint nounce,
-        uint deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external returns (bool) {
-        
+     function permitBuy(uint256 nonce, bytes calldata signature, uint256 tokenId) public {
+        _useCheckedNonce(msg.sender, nonce);
 
-        ERC2612-NFT(nftAddress).permit(from, to, nftId, nounce, deadline, v, r, s);
+        bytes32 hash = keccak256(abi.encodePacked(msg.sender, nonce));
+        hash = hash.toEthSignedMessageHash();
+        address signAddr = hash.recover(signature);
+        require(signAddr == alice, "error signiture");
 
-        prices[nftId] = price;
-        seller[nftId] = from;
-        emit Listed(from, price);
+        _useNonce(msg.sender);
 
-        ERC2612(tokenAddress).transferFrom(msg.sender, address(this), price);
-        ERC2612(tokenAddress).transfer(to, price);
-
-        ERC2612-Nft(nftAddress).transferFrom(address(this), msg.sender, nftId);
-        emit Sold(sellers[nftId], msg.sender, prices[nftId]);
-        delete prices[nftId];
-        delete sellers[nftId];
-        return true;
+        buyNFT(tokenId);
     }
     
 
-    //拓展token回调
-    function tokensReceived(address buyer, uint256 amount, bytes memory ExtraData) public returns (bool) {
-        require(msg.sender == _erc20addr, "buyNFT_callback function should be called by ERC20 contract!");
-        uint256 tokenId = abi.decode(ExtraData, (uint256));
-        Listing memory listing = listings[tokenId];
-        require(listing.seller != address(0), "NFT is not listed for sale");
-        require(amount == listing.price, "Amount must equal token price!");
-        uint256 price = listing.price;
-        address seller = listing.seller;
-        nftContract.safeTransferFrom(seller, buyer, tokenId);
-        tokenContract.transfer(seller, amount);
-        delete listings[tokenId];
-        emit NFTSold(tokenId, msg.sender, price);
-        return true;
-    }
+
 }
