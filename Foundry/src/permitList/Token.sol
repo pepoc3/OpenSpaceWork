208a0d3e02e269ceab5b5ea5ff22c21ba15f1078
pepoc3 <996473441@qq.com>
2024-05-07 22:23:02 +0800

    Modify Token.sol

--- Foundry/src/permitList/Token.sol
+++ Foundry/src/permitList/Token.sol
@@ -1,11 +1,32 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+//SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 
-contract Token is ERC20 { 
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        _mint(msg.sender, 1e10*1e18);
-    } 
+
+interface TokenRecipient {
+    function tokensReceived(address sender, uint amount) external returns (bool);
+}   
+
+contract Token is ERC20Permit {
+    using Address for address;
+
+    constructor() ERC20("Pepoc3-ERC2612", "Pepoc3-ERC2612") ERC20Permit("Pepoc3-ERC2612") {
+        _mint(msg.sender, 2612 * 10 ** 18);
+    }
+
+    function transferWithCallback(address recipient, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+
+        if (recipient.code.length > 0) {
+            bool rv = TokenRecipient(recipient).tokensReceived(msg.sender, amount);
+            require(rv, "No tokensReceived");
+        }
+
+        return true;
+    }
+    
 }
