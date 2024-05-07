fee3ac2fdf27ab05d51274f0dc75fcc3f62f351c
pepoc3 <996473441@qq.com>
2024-05-07 22:22:04 +0800

    Modify NFT.sol

--- Foundry/src/permitList/NFT.sol
+++ Foundry/src/permitList/NFT.sol
@@ -7,24 +7,24 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    bytes32 public DOMAIN_SEPARATER;
+     bytes32 public DOMAIN_SEPARATER;
     mapping(address => uint) public nounces;
     uint256 tokenId;
 
     constructor() ERC721(unicode"MyNft", "NFT") {}
 
-    function mint(address user) external returns (bool) {
-        _mint(user, tokenId);
+    function mint() external returns (bool) {
+        _mint(msg.sender, tokenId);
         tokenId += 1;
         return true;
     }
     function permit(
         address from,
         address to,
-        uint nftId,
-        uint price,
-        uint nounce,
-        uint deadline,
+        uint256 nftId,
+        uint256 price,
+        uint256 nounce,
+        uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
@@ -36,12 +36,11 @@ contract NFT is ERC721URIStorage {
                 keccak256(
                     abi.encode(
                         keccak256(
-                            "Permit(address from,address to,uint256 nftId,uint256 nonce,uint256 deadline)"
+                            "Permit(address from,address to,uint256 nftId,uint256 price,uint256 nonce,uint256 deadline)"
                         ),
                         from,
                         to,
                         nftId,
-                        price,
                         nounce,
                         deadline
                     )
